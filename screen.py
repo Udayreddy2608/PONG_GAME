6268111cd073bf2c17e053c4cbd32d501d015778
Uday Kiran Reddy Saddipally <143345142+Udayreddy2608@users.noreply.github.com>
2023-10-30 20:48:44 +0530

    Update screen.py

--- screen.py
+++ screen.py
@@ -2,8 +2,8 @@ from turtle import Turtle
 
 class MidLine:
     def __init__(self):
-        self.niki=Turtle("classic")
-        self.niki.color("White")
-        self.niki.goto(0,300)
-        self.niki.goto(0,-300)
+        self.uday=Turtle("classic")
+        self.uday.color("White")
+        self.uday.goto(0,300)
+        self.uday.goto(0,-300)
 
